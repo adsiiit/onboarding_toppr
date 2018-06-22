741afa407b3fc26fe6e2133762026a87a0bf71e5
Amandeep Sachan <amandeep.sachan@toppr.com>
2018-06-22 19:20:13 +0530

    code with pep8 standard

--- python_basics.py
+++ python_basics.py
@@ -142,10 +142,34 @@
 
 
 ## User-defined exceptions
-class Networkerror(RuntimeError):
-   def __init__(self, arg):
-      self.args = arg
-try:
-   raise Networkerror("Bad hostname")
-except Networkerror,e:
-   print e.args
+# class Networkerror(RuntimeError):
+#    def __init__(self, arg):
+#       self.args = arg
+# try:
+#    raise Networkerror("Bad hostname")
+# except Networkerror,e:
+#    print e.args
+
+
+
+## Code as per PEP8 standards
+
+# Python program to find the factorial of a number provided by the user.
+ 
+# change the value for a different result
+num = 7
+ 
+# uncomment to take input from the user
+#num = int(input("Enter a number: "))
+ 
+factorial = 1
+ 
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
